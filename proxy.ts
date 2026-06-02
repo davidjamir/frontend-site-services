c41f3e047b8f303273e781294b9aff6c7c34e308
Hoa Duc Viet <hoaducviet@Viet-MBP.local>
2026-06-02 16:21:06 +0700

    Add rewrite cdn /feed for url from blogger

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,15 @@
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export function proxy() {
+export function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/feeds/posts/default") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/feed";
+
+    return NextResponse.rewrite(url);
+  }
+
   return NextResponse.next();
 }
