18465348b34d1652bdfe8cd528e3c3630be055c7
Hoa Duc Viet <viethoaduc.21@gmail.com>
2026-09-25 19:02:51 +0700

    Update cors by other method http and https

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
     unoptimized: false,
     remotePatterns: [
       {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
         protocol: "https",
         hostname: "**",
       },
